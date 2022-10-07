4f34391740b78b9571393c77468ebb8bfbccd03e
Maxawergy <maxawergy@yandex.ru>
2022-10-07 15:09:29 +0300

    Test Delete 2 Passed

--- tests.py
+++ tests.py
@@ -1,9 +1,13 @@
 from shredder import Shredder
+shred = Shredder()
 
 def test_delete_1():
-    shred = Shredder()
     assert shred.delete_file() == False
+
+def test_delete_2():
+    assert shred.delete_file(path="TEST_DIR/k1.txt") == True
 
 if __name__ == "__main__":
     test_delete_1()
+    test_delete_2()
     print("OK")
