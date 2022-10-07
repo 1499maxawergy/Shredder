0a10cc88495be9e4bb681e0ad5fcca542e1197f7
Maxawergy <maxawergy@yandex.ru>
2022-10-07 15:03:31 +0300

    Test Delete 1 Passed

--- shredder.py
+++ shredder.py
@@ -4,4 +4,8 @@ import os
 class Shredder:
     #Удаление файла без затирки (можно восстановить)
     def delete_file(self, path=None):
-        pass
+        try:
+            os.remove(path=path)
+            return True
+        except Exception:
+            return False
