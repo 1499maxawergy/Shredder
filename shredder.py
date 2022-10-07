1e1b316a4330b3f9d970fd80c7f185b3bebdff38
Maxawergy <maxawergy@yandex.ru>
2022-10-07 16:53:02 +0300

    Test Wipe 1 Passed Test Wipe 2 Passed Test Wipe 3

--- shredder.py
+++ shredder.py
@@ -1,6 +1,11 @@
 import os
+import string
+import random
 
 class Shredder:
+
+    slash = '/'
+
     #Удаление файла без затирки (можно восстановить)
     def delete_file(self, path=None):
         try:
@@ -23,6 +28,36 @@ class Shredder:
         except Exception:
             return False
 
-    
-    def wipe_file(self, path=None):
+    #Затирка файла (восстановить нельзя)
+    def wipe_file(self, path=None, level=1):
+        try:
+            valid_chars = string.ascii_letters + string.digits
+            filesize = os.path.getsize(path)
+            num_lines = sum(1 for line in open(path))
+            if(os.path.isfile(path) == True and filesize > 0):
+                for temp in range(level):
+                    #Записываем в файл всевозможные символы
+                    with open(path, 'w') as f:
+                        for i in range(num_lines):
+                            string_writable = "".join(random.choices(valid_chars, k=filesize)) + '\n'
+                            f.write(string_writable)
+
+                    #Переименовываем файл
+                    new_name = "".join(random.choices(valid_chars, k=random.choice(range(1, 27))))
+                    new_path = f"{self.slash}".join(path.split(f"{self.slash}")[0:-1]) + f"{self.slash}{new_name}"
+                    if(len(path.split(f"{self.slash}")) == 1):
+                        new_path = new_name
+                    os.rename(path, new_path)
+                    path = new_path
+                
+                #Удаляем файл после вайпа
+                os.remove(path)
+                return True
+            else:
+                return False
+        except Exception:
+            return False
+
+
+    def wipe_multiple(self, path=None):
         pass
--- tests.py
+++ tests.py
@@ -14,12 +14,19 @@ def test_delete_4():
     assert shred.delete_muptiple(path="TEST_DIR") == True
 
 def test_wipe_1():
-    assert shred.wipe_file(path="k1.txt") == True
+    assert shred.wipe_file(path="k1.txt") == False
+
+def test_wipe_2():
+    assert shred.wipe_file(path="TEST_DIR/k1.txt") == True
+
+def test_wipe_3():
+    assert shred.wipe_multiple(path="TEST_DIR") == True
 
 if __name__ == "__main__":
-    test_delete_1()
-    test_delete_2()
-    test_delete_3()
-    test_delete_4()
+    # test_delete_1()
+    # test_delete_2()
+    # test_delete_3()
+    # test_delete_4()
     test_wipe_1()
+    test_wipe_2()
     print("OK")
