a15be9d8dea85ed573c1d9c401374e124dccc016
Maxawergy <maxawergy@yandex.ru>
2022-10-07 15:13:34 +0300

    Test Delete 3 Passed Test Delete 4

--- tests.py
+++ tests.py
@@ -7,7 +7,15 @@ def test_delete_1():
 def test_delete_2():
     assert shred.delete_file(path="TEST_DIR/k1.txt") == True
 
+def test_delete_3():
+    assert shred.delete_file(path="KKKSSSS/KISKIS.txt") == False
+
+def test_delete_4():
+    assert shred.delete_file(path="TEST_DIR") == True
+
 if __name__ == "__main__":
     test_delete_1()
     test_delete_2()
+    test_delete_3()
+    test_delete_4()
     print("OK")
